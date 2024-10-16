fc3bd2db1e83af6db962de345bb85273c330afcc
Raaja Das <117619274+Concave-Streak@users.noreply.github.com>
2024-10-16 21:19:55 +0530

    updated assembler

--- Verilog Assets/Programs/gcd.asm
+++ Verilog Assets/Programs/gcd.asm
@@ -3,11 +3,11 @@ num1:  .int 36 # First number
 num2:  .int 60 # Second number
 
 .text
-ld $1 num1 # Load num1 into $1
-ld $2 num2 # Load num2 into $2
+ld $1 num1 # Load num1 into Register 1
+ld $2 num2 # Load num2 into Register 2
 
 gcd_loop:
-    sub $c $1 $2
+    sub $c $1 $2 
     bz $c end
 
     sgt $c $1 $2
@@ -22,4 +22,3 @@ less_than:
 end:
     move $d $1
     halt
-
