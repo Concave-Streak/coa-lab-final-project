6af3f6bcd5aaa7512f9f38a6060fffaf3cd597ec
Raaja Das <117619274+Concave-Streak@users.noreply.github.com>
2024-10-30 15:23:32 +0530

    sum of n natural nums

--- Programs/counter.s
+++ Programs/counter.s
@@ -1,18 +1,31 @@
 .data
-a: .int 100
-b: .int 0
+prompt: .str "Enter an Integer: "
+nl: .str "\n"
+heap: .int 0
 
 .text
-ld $1 a($0)
 
-li $2 -3
-li $3 10
+la $a prompt
+jal prints
+
+jal getint
+move $1 $fo
+
+li $2 1
+li $3 0
 
 while:
-    bz $3 done
-    add $1 $1 $2
-    subi $3 $3 1
+    bz $1 done
+
+    add $3 $3 $2
+    addi $2 $2 1
+    subi $1 $1 1
+
     br while
 done:
-    st $1 b($0)
-    halt
+    move $a $3
+    jal printi
+
+    la $a nl
+    jal prints
+    jr $0
